b7edf88d1a1cad49611d73f00d73113f15117ac8
Geethanjali346 <111630167+Geethanjali346@users.noreply.github.com>
2023-06-23 13:09:21 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+for (let i = 1; i <= 100; i++) {
+  if (i % 3 === 0 && i % 5 === 0) {
+    console.log('amajon');
+  } else if (i % 3 === 0) {
+    console.log('google');
+  } else if (i % 5 === 0) {
+    console.log('facebook');
+  } else {
+    console.log(i);
+  }
+}
